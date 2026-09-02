7afbec07fa3bd1808c1a7926f4d511a91f50c628
Andrey Kuvshinov <andrew@encloud.blue>
2026-09-03 00:36:59 +0700

    sync from monorepo e13ae3a
    Source: github.com/everanium/itb @ e13ae3acf4d4f3dad4b0b05b4fe97eec2f93e869

--- src/Itb.php
+++ src/Itb.php
@@ -26,7 +26,7 @@ namespace Everanium\Itb;
 final class Itb
 {
     /** Binding version. */
-    public const VERSION = '0.3.1';
+    public const VERSION = '0.3.2';
 
     /** Floor capacity for blob output buffers (create / rekey). */
     public const BLOB_CAP = 65536;
